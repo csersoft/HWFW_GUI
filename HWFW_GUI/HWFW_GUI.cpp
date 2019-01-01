1d363cc57ee4b045b14bf16da32153dbe9d987dc
csersoft <csersoft@gmail.com>
2019-01-01 16:43:39 +0800

    190111

--- HWFW_GUI/HWFW_GUI.cpp
+++ HWFW_GUI/HWFW_GUI.cpp
@@ -35,8 +35,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
   UNREFERENCED_PARAMETER(hPrevInstance);
   UNREFERENCED_PARAMETER(lpCmdLine);
 
-  InitCommonControls();
   _CrtSetDebugFillThreshold(0);
+  InitCommonControls();
 
   // 执行应用程序初始化: 
   if (!InitInstance(hInstance, nCmdShow))
