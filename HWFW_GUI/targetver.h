0763e37cc97a376817f6297c21f12ef0e69f22bc
emersonfelipesp <emerson.felipe@nmultifibra.com.br>
2022-03-18 18:07:43 -0300

    translate targetver.h

--- HWFW_GUI/targetver.h
+++ HWFW_GUI/targetver.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
 
-// 包括 SDKDDKVer.h 将定义可用的最高版本的 Windows 平台。
+// Including SDKDDKVer.h will define the highest version of the Windows platform available.
 
-// 如果要为以前的 Windows 平台生成应用程序，请包括 WinSDKVer.h，并将
-// 将 _WIN32_WINNT 宏设置为要支持的平台，然后再包括 SDKDDKVer.h。
+// If you are building applications for previous Windows platforms, include WinSDKVer.h, and add
+// Set the _WIN32_WINNT macro to the platform to support before including SDKDDKVer.h.
 
 #include <SDKDDKVer.h>
